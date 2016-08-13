2c881ccb08c97d0e20118574a5e38ad096855df3
Pedro Costa <dev@iampfac.com>
2016-08-13 15:08:51 +0100

    Contacts panel
    Why:
    
    * People need to know how to contact us.
    
    These changes address the need by:
    
    * Adding icons for each contact type;
    * Adding a style component to properly format phone numbers;
    * Adding a style component to format the contacts lists;
    * Adding a style component to layou the contacts panel content;
    * Refactoring spacings, typography and colors into helpers that allow
      simplyfying the stylesheets.

--- config.rb
+++ config.rb
@@ -31,13 +31,6 @@ end
 # Helpers
 ###
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
